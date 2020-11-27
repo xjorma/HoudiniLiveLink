ee77202a4becc4bddffa6ac6eba487aa9d5b601d
David Gallardo <xjormaeko@gmail.com>
2020-11-27 15:51:18 -0500

    First commit, add 2 houdini projects, completed leap motion, need to finish oculus quest

---
+++ Unity/LeapMotionProvider/Assets/Script/HandUDP.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class HandUDP : MonoBehaviour
+{
+    public GameObject[] roots;
+    public string       destinationAdress;
+    public int          destinationPort;
+
+    UdpClient udpClient;
+
+    List<GameObject> list;
+    public void GetAllChilds(GameObject Go, List<GameObject> list)
+    {
+        list.Add(Go);
+        //Debug.Log(Go.name);
+        for (int i = 0; i < Go.transform.childCount; i++)
+        {
+            GameObject curGO = Go.transform.GetChild(i).gameObject;
+            GetAllChilds(curGO, list);
+        }
+    }
+
+    void Start()
+    {
+        list = new List<GameObject>();
+        foreach(GameObject root in roots)
+        {
+            GetAllChilds(root, list);
+        }
+        udpClient = new UdpClient();
+    }
+
+    void Update()
+    {
+        List<string> values = new List<string>();
+        foreach (GameObject go in list)
+        {
+            Vector3 position = go.transform.position;
+            Quaternion rotation = go.transform.rotation;
+            values.Add(go.name);
+            values.Add(position.x.ToString());
+            values.Add(position.y.ToString());
+            values.Add(position.z.ToString());
+            values.Add(rotation.x.ToString());
+            values.Add(rotation.y.ToString());
+            values.Add(rotation.z.ToString());
+            values.Add(rotation.w.ToString());
+        }
+        string finalString = string.Join("|", values);
+        byte[] sendBytes = Encoding.UTF8.GetBytes(finalString);
+        //byte[] sendBytes = Encoding.ASCII.GetBytes(finalString);
+        udpClient.Send(sendBytes, sendBytes.Length, destinationAdress, destinationPort);
+    }
+}
---
+++ Unity/OculusQuestProvider/Assets/Script/HandUDP.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class HandUDP : MonoBehaviour
+{
+    public GameObject   root;
+    public string       destinationAdress;
+    public int          destinationPort;
+    
+    UdpClient udpClient;
+    List<Transform> list;
+
+    string[] boneNames = {"b_l_wrist", "b_l_thumb1", "b_l_thumb2", "b_l_thumb3", "b_l_index1", "b_l_index2", "b_l_index3", "b_l_middle1", "b_l_middle2", "b_l_middle3", "b_l_ring1", "b_l_ring2", "b_l_ring3", "b_l_pinky0", "b_l_pinky1", "b_l_pinky2", "b_l_pinky3" };
+    
+    Transform RecursiveFind(Transform transform, string name)
+    {
+        foreach(Transform childTrans in transform)
+        {
+            if(childTrans.name == name)
+            {
+                return childTrans;
+            }
+            else
+            {
+                Transform found = RecursiveFind(childTrans, name);
+                if(found != null)
+                {
+                    return found;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public void GetAllChilds(GameObject Go, List<Transform> list)
+    {
+        Debug.Log(Go.name);
+        foreach (string boneName in boneNames)
+        {
+            Transform found = RecursiveFind(Go.transform, boneName);
+            if (found != null)
+            {
+                list.Add(found);
+            }
+            else
+            {
+                Debug.Log(Go.name + " not found!");
+            }
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log("starting the shit");
+        list = new List<Transform>();
+        GetAllChilds(root, list);
+        udpClient = new UdpClient();
+    }
+
+    void Update()
+    {
+        List<string> values = new List<string>();
+        foreach (Transform tr in list)
+        {
+            Vector3 position = tr.position;
+            Quaternion rotation = tr.rotation;
+            values.Add(tr.name);
+            values.Add(position.x.ToString());
+            values.Add(position.y.ToString());
+            values.Add(position.z.ToString());
+            values.Add(rotation.x.ToString());
+            values.Add(rotation.y.ToString());
+            values.Add(rotation.z.ToString());
+            values.Add(rotation.w.ToString());
+        }
+        string finalString = string.Join("|", values);
+        Debug.Log(finalString);
+        byte[] sendBytes = Encoding.UTF8.GetBytes(finalString);
+        udpClient.Send(sendBytes, sendBytes.Length, destinationAdress, destinationPort);
+        Debug.Log("Shit sent");
+    }
+}
